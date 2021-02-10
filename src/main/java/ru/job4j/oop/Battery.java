66b5e2d3b6a737867b7a2b5f99a24744201d19fc
Asus <v_budaev78@mail.ru>
2021-02-10 15:43:44 +0400

    1.8. Состояние объекта [#458768]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load){
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(100);
+        Battery second = new Battery(50);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        second.exchange(first);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
+}
