6a8f18bae82ed8622f5970badef902b2263b683f
Asus <v_budaev78@mail.ru>
2021-02-10 14:29:43 +0400

    1.4. Вызов метода с возвращаемым типом. [#458767]

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+import java.sql.SQLOutput;
+import java.util.SortedMap;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        return "Неизвестное слово " + eng;
+    }
+}
