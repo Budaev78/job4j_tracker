be2a80e57c6d5984968cf7380a7ff6520f2a4f3f
Asus <v_budaev78@mail.ru>
2021-02-10 15:16:19 +0400

    1.7. Конструктор [#458769]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Fix {
+
+    private String desc;
+
+    public Fix (String desc) {
+        this.desc = desc;
+    }
+
+    public static void main(String[] args) {
+        String issue = "Fix me ASAP.";
+        Fix fix = new Fix(issue);
+    }
+}
