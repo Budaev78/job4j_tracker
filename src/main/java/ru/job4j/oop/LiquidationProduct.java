66c63ce61ff50cdc60f88b786b3ba8a880f014c5
Asus <v_budaev78@mail.ru>
2021-02-15 12:41:09 +0400

    6. Почему наследование - это плохо [#458777]

---
+++ src/main/java/ru/job4j/oop/LiquidationProduct.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public final class LiquidationProduct{
+
+    private Product product;
+
+    public LiquidationProduct(String name, int price) {
+        product = new Product(name, price);
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public String label() {
+        return product.label();
+    }
+}
