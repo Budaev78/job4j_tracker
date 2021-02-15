05c2a82a5ad13a3608b3d97c7552583a3577d259
Asus <v_budaev78@mail.ru>
2021-02-15 14:35:58 +0400

    2. Модель данных. [#458782]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -7,7 +7,6 @@ public class College {
         student.setName("Владимир Будаев");
         student.setGroup("654 группа");
         student.setDate("01.02.2021г.");
-        System.out.println("Студент " + student.getName() + " из " + student.getGroup() + " поступил на курсы "
-                + student.getDate());
+        System.out.println("Студент " + student.getName() + " из " + student.getGroup() + " поступил на курсы " + student.getDate());
     }
 }
