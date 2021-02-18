7ec76d97f2164bc987a35c5a36f04b1c07a4d026
Asus <v_budaev78@mail.ru>
2021-02-18 15:56:27 +0400

    3. Интерфейс Input [#458713]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void passenger(int count) {
+
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    public void move();
+
+    public void passenger(int count);
+
+    public int refuel(int fuel);
+}
