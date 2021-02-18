324d1470b3866e6a868e3ce1abe58c4b18b33400
Asus <v_budaev78@mail.ru>
2021-02-18 20:46:51 +0400

    4.1. Разрыв зависимости StartUI от Scanner. [#458717]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,57 +6,81 @@ import java.util.Arrays;
 
 public class StartUI {
 
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void showItem(Tracker tracker) {
+        Item [] lists = tracker.findAll();
+        for (Item list : lists) {
+            System.out.println(list);
+        }
+    }
+
+    public static void replaceItem(Input input, Tracker tracker) {
+        int id = Integer.valueOf(input.askStr("Enter the application number: "));
+        String name = input.askStr("Enter a new application name: ");
+        Item newItem = new Item(name);
+        Item rep = tracker.replace(id, newItem);
+        if (rep != null) {
+            System.out.println("The application has been successfully changed");
+        } else {
+            System.out.println("Application number not found");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        int id = Integer.valueOf(input.askStr("Enter the application number: "));
+        tracker.delete(id);
+        if (tracker.delete(id)) {
+            System.out.println("The application has been successfully deleted");
+        } else {
+            System.out.println("Application number not found");
+        }
+    }
+
+    public static void findIdItem(Input input, Tracker tracker) {
+        int id = Integer.valueOf(input.askStr("Enter the application number: "));
+        Item item = tracker.findById(id);
+        if (item != null) {
+            System.out.println(item);
+        } else {
+            System.out.println("Application number not found");
+        }
+    }
+
+    public static void findNameItem(Input input, Tracker tracker) {
+        String name = input.askStr("Enter the name of the request: ");
+        Item[] itemNames = tracker.findByName(name);
+        if (itemNames.length > 0) {
+            for (Item names : itemNames) {
+                System.out.println(names);
+            }
+        } else {
+            System.out.println("Applications with this name were not found");
+        }
+    }
+
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
             int select = Integer.valueOf(input.askStr("Select: "));
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
-                Item[] lists = tracker.findAll();
-                for (Item list : lists) {
-                    System.out.println(list);
-                }
+                StartUI.showItem(tracker);
             } else if (select == 2) {
-                int id = Integer.valueOf(input.askStr("Enter the application number: "));
-                String name = input.askStr("Enter a new application name: ");
-                Item newItem = new Item(name);
-                Item rep = tracker.replace(id, newItem);
-                if (rep != null) {
-                    System.out.println("The application has been successfully changed");
-                } else {
-                    System.out.println("Application number not found");
-                }
+                StartUI.replaceItem(input, tracker);
             } else if (select == 3) {
-                int id = Integer.valueOf(input.askStr("Enter the application number: "));
-                tracker.delete(id);
-                if (tracker.delete(id)) {
-                    System.out.println("The application has been successfully deleted");
-                } else {
-                    System.out.println("Application number not found");
-                }
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
-                int id = Integer.valueOf(input.askStr("Enter the application number: "));
-                Item item = tracker.findById(id);
-                if (item != null) {
-                    System.out.println(item);
-                } else {
-                    System.out.println("Application number not found");
-                }
+                StartUI.findIdItem(input, tracker);
             } else if (select == 5) {
-                String name = input.askStr("Enter the name of the request: ");
-                Item[] itemNames = tracker.findByName(name);
-                if (itemNames.length > 0) {
-                    for (Item names : itemNames) {
-                        System.out.println(names);
-                    }
-                } else {
-                    System.out.println("Applications with this name were not found");
-                }
+                StartUI.findNameItem(input, tracker);
             } else if (select == 6) {
                 run = false;
             }
