d98967f4e327ee9027e2237c4e336cdd4a68f13d
Asus <v_budaev78@mail.ru>
2021-02-28 18:40:26 +0400

    9. Написать тесты на StartUI. [#458724]

--- src/main/java/ru/job4j/tracker/StubInput.java
+++ src/main/java/ru/job4j/tracker/StubInput.java
@@ -16,6 +16,6 @@ public class StubInput implements Input{
 
     @Override
     public int askInt(String question) {
-        return Integer.valueOf(askStr(question));
+        return Integer.parseInt(askStr(question));
     }
 }
