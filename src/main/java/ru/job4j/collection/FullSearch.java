746f48a1f19cb21c59d67f92cad9970fb758c9f6
Asus <v_budaev78@mail.ru>
2021-03-12 16:47:34 +0400

    1. Уникальные задачи. [#458703]

--- src/main/java/ru/job4j/collection/FullSearch.java
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -9,14 +9,8 @@ public class FullSearch {
 
     public static HashSet<String> extractNumber(List<Task> list) {
         HashSet<String> numbers = new HashSet<>();
-        for (int i = 0; i < list.size(); i++) {
-            for(int j = i + 1; j < list.size(); j++) {
-                if(list.get(i).equals(list.get(j))){
-                    list.remove(j);
-                    j--;
-                }
-            }
-           numbers.add(list.get(i).getNumber());
+        for (Task task : list) {
+            numbers.add(task.getNumber());
         }
         return numbers;
     }
