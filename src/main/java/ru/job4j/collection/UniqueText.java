2038cc9c16b176d7db22819ceca4ae92e59d1ef1
Asus <v_budaev78@mail.ru>
2021-03-12 16:19:33 +0400

    2. Идентичные тексты [#458704]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        /* for-each origin -> new HashSet. */
+        /* for-each text -> hashSet.contains */
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String tex : text) {
+            if(!check.contains(tex)){
+                return false;
+            }
+        }
+        return rsl;
+    }
+}
