32122d8f67a523416b2cdad793b450ee59547b76
Asus <v_budaev78@mail.ru>
2021-03-12 09:11:33 +0400

    4. Конвертация двумерного массива в ArrayList [#458692]

--- src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
+++ src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -8,7 +8,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ConvertMatrix2ListTest {
-
     @Test
     public void when2on2ArrayThenList4() {
         ConvertMatrix2List list = new ConvertMatrix2List();
