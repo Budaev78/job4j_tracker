1cfbef7e2c0fdc21b6676a64ae55951c23bed7f2
Asus <v_budaev78@mail.ru>
2021-03-12 08:49:28 +0400

    2. Очередь с приоритетом на LinkedList [#458691]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -11,6 +11,8 @@ public class PriorityQueue {
         for (Task element : tasks) {
             if(element.getPriority() < task.getPriority()) {
                 index += 1;
+            } else {
+                break;
             }
         }
         this.tasks.add(index, task);
