de2910384c450af0fed0113057e18fcdc943106a
Asus <v_budaev78@mail.ru>
2021-03-16 15:57:37 +0400

    0. Сортировка [#458688]

---
+++ src/main/java/ru/job4j/tracker/ItemSorter.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ItemSorter {
+
+    public static void main(String[] args) {
+        List<Item> items = Arrays.asList(
+                new Item(3,"car"),
+                new Item(1, "fly"),
+                new Item(6, "train")
+        );
+        System.out.println(items);
+        Collections.sort(items);
+        System.out.println(items);
+        Collections.sort(items, Collections.reverseOrder());
+        System.out.println(items);
+        Collections.sort(items, new SortByNameItem());
+        System.out.println(items);
+        Collections.sort(items, new SortByNameReverseItem());
+        System.out.println(items);
+
+    }
+}
