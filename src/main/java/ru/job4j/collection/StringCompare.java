a189fa01003ba0cae44d648a922ea2282d396287
Asus <v_budaev78@mail.ru>
2021-03-19 16:16:36 +0400

    3. Компаратор для строк [#458684]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        for (int i = 0; i < left.length(); i++) {
+            if (Character.compare(left.charAt(i), right.charAt(i)) != 0) {
+                return Character.compare(left.charAt(i), right.charAt(i));
+            }
+        }
+        return 0;
+    }
+}
