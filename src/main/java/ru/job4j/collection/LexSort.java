76e8e56c0a714372511a266eaa8bb9161b2a7567
Asus <v_budaev78@mail.ru>
2021-03-22 18:34:59 +0400

    4. Сортировка номера [#458685]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,6 +6,10 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        return left.compareTo(right);
+        String[] arLeft = left.split("\\.");
+        String[] arRight = right.split("\\.");
+        int intLeft = Integer.parseInt(arLeft[0]);
+        int intRight = Integer.parseInt(arRight[0]);
+        return Integer.compare(intLeft, intRight);
     }
 }
