5b2d550df68a2be0ade97a3d37fd5164d8cdb71a
Asus <v_budaev78@mail.ru>
2021-03-30 15:24:07 +0400

    2. Проверить две строчки на идентичность.

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -7,6 +7,7 @@ public class Article {
     public static boolean generateBy(String origin, String line) {
         boolean rsl = true;
         String orig = origin.replaceAll("\\p{P}", "");
+        String lin = line.replaceAll("\\p{P}", "");
         String[] originText = orig.split(" ");
         String[] lineText = line.split(" ");
         HashSet<String> check = new HashSet<>(Arrays.asList(originText));
