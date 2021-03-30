6ec5a2577abf0dadb00664662d9ab911fa724a19
Asus <v_budaev78@mail.ru>
2021-03-30 14:37:14 +0400

    2. Проверить две строчки на идентичность.

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,17 +1,28 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        String[] arLeft = left.split("");
-        String[] arRight = right.split("");
-        List<String> listLeft = Arrays.asList(arLeft);
-        List<String> listRight = Arrays.asList(arRight);
-        Collections.sort(listLeft);
-        Collections.sort(listRight);
-        return listLeft.equals(listRight);
+        Map<Character, Integer> leftMap = new HashMap<>();
+        char[] arLeft = left.toCharArray();
+        for (char item : arLeft) {
+            int count = leftMap.getOrDefault(item, 0);
+            leftMap.put(item, count + 1);
+        }
+        char[] arRight = right.toCharArray();
+        for (char c : arRight) {
+            Integer value = leftMap.get(c);
+            if (leftMap.containsKey(c) && value == 1) {
+                leftMap.remove(c);
+            }
+            if (leftMap.containsKey(c) && value > 1) {
+                leftMap.put(c, value - 1);
+            }
+            if (leftMap.size() == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
