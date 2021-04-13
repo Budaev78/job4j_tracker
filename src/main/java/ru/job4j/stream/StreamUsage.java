eaf22a9b5b1ee315ed95cdb9250cda4ed67b2831
Asus <v_budaev78@mail.ru>
2021-04-13 18:10:37 +0400

    0. Stream API [#458522]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -8,9 +8,10 @@ import java.util.stream.Collectors;
 public class StreamUsage {
 
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 16, 20);
-        List<Integer> evenNum = numbers.stream().filter(
-                num -> num % 2 == 0
+        List<Integer> numbers = Arrays.asList(-1, 2, 3, -4, 5, 6, -7, -8, 9, -11, -15, 16, 20);
+        List<Integer> positiveNum = numbers.stream().filter(
+                num -> num > 0
         ).collect(Collectors.toList());
+        System.out.println(positiveNum);
     }
 }
