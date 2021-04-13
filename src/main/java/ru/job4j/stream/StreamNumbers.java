d601a2b494924d8c6cd6eaf7732338c25b025434
Asus <v_budaev78@mail.ru>
2021-04-13 18:15:26 +0400

    0. Stream API [#458522]

--- src/main/java/ru/job4j/stream/StreamNumbers.java
+++ src/main/java/ru/job4j/stream/StreamNumbers.java
@@ -5,13 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamUsage {
+public class StreamNumbers {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(-1, 2, 3, -4, 5, 6, -7, -8, 9, -11, -15, 16, 20);
         List<Integer> positiveNum = numbers.stream().filter(
                 num -> num > 0
         ).collect(Collectors.toList());
-        System.out.println(positiveNum);
     }
 }
