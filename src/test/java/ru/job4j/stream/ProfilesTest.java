0f45feef0530ab809f8b43eb84e54d5393a1d15e
Asus <v_budaev78@mail.ru>
2021-04-14 17:08:51 +0400

    3. Уникальность элементов и сортировка. [#458525]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -6,10 +6,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
 
@@ -17,10 +15,18 @@ public class ProfilesTest {
     public void testCollect() {
         Address adr1 = new Address("Moscow", "Lenina", 12, 26);
         Address adr2 = new Address("Moscow", "Lenina", 14, 28);
-        List<Profile> profiles = Arrays.asList(new Profile(adr1), new Profile(adr2));
+        Address adr3 = new Address("SPb", "Popova", 24, 15);
+        Address adr4 = new Address("Kazan", "Novgorodskaya", 2, 102);
+        Address adr5 = new Address("SPb", "Chkalovskiy", 35, 3);
+        List<Address> addressList = Arrays.asList(adr1, adr2, adr3, adr4, adr5);
+        addressList.sort(new AddressSortByCity());
+        List<Profile> profiles = new ArrayList<>();
+        for (Address address : addressList) {
+            profiles.add(new Profile(address));
+        }
         Profiles prof = new Profiles();
         List<Address> result = prof.collect(profiles);
-        List<Address> expected = Arrays.asList(adr1, adr2);
+        List<Address> expected = Arrays.asList(adr4, adr1, adr2, adr3, adr5);
         assertThat(result, Matchers.is(expected));
     }
 }
